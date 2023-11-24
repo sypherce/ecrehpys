a8accde60739b39c19bbca9dc70b74f71ade5ea4
sypherce <sypherce@gmail.com>
2023-11-24 06:21:37 -0800

    changed all " to '. using path.join

--- frontend.js
+++ frontend.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.listen(3000, () => {
-	console.log("Application started and Listening on port 3000");
+	console.log('Application started and Listening on port 3000');
 });
+
+
+
+/*app.get('/', function(req,res) {
+	res.sendFile(path.join(__dirname, '/alerts/index.html');
+});*/
 
 // server css as static
-app.use(express.static(__dirname + "/alerts"));
+app.use('/', express.static(path.join(__dirname, '/alerts')));
 
+app.use('/peaks/', express.static(path.join(__dirname, '/../stream')));
 // get our app to use body parser
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/alerts/index.html");
-});
