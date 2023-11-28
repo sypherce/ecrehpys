c0441918301cc0ac34df4e42464cf906b06e87a1
sypherce <sypherce@gmail.com>
2023-11-28 04:26:08 -0800

    updated for mistates project

--- frontend.js
+++ frontend.js
@@ -1,21 +1,17 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
+app.use(cors())
 
 app.listen(3000, () => {
 	console.log('Application started and Listening on port 3000');
 });
 
-
-
-/*app.get('/', function(req,res) {
-	res.sendFile(path.join(__dirname, '/alerts/index.html');
-});*/
-
-// server css as static
 app.use('/', express.static(path.join(__dirname, '/alerts')));
 
 app.use('/peaks/', express.static(path.join(__dirname, '/../stream')));
+app.use('/mistates/', express.static(path.join(__dirname, '/../../../projects/mistates/')));
 // get our app to use body parser
 app.use(bodyParser.urlencoded({ extended: true }))
